9075b65e0068e66ac049e9a398c4c2a96dbdf455
Karen <karendavtyan21@mail.com>
2019-10-17 19:26:43 +0000

    added getAllPropreties that work with sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -93,12 +93,43 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => res.rows);
-}
+    const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+    if(options.owner_id) {
+      queryParams.push(`%${options.owner_id}%`)
+      queryString += `AND owner_id = $${queryParams.length}`
+    }
+    if(min(options.minimum_price_per_night)) {
+      queryParams.push(`%${options.minimum_price_per_night}%`)
+      queryString += `AND cost_per_night >= ${queryParams.length}`
+    }
+    if(max(options.maximum_price_per_night)) {
+      queryParams.push(`%${options.maximum_price_per_night}%`)
+      queryString += `AND cost_per_night <= ${queryParams.length}`
+    }
+
+    if(options.minimum_rating) {
+      queryParams.push(`%${options.minimum_rating}%`)
+      queryString += `AND rating >= ${queryParams.length}`
+    }
+    queryString +=  `GROUP BY properties.id`
+    queryString +=  `ORDER BY cost_per_night`
+    queryParams.push(limit);
+    queryString += `
+    LIMIT ${queryParams.length};
+    `;
+    console.log(queryString, queryParams);
+    return pool.query(queryString, queryParams)
+    .then(res => res.rows);
+  }
 
 exports.getAllProperties = getAllProperties;
 
